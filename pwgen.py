10ee45634a27b3aa3979ff9f052cf58a0e11e95e
ccdale <chris.allison@hivehome.com>
2020-11-25 15:39:31 +0000

    mark capital letters

--- pwgen.py
+++ pwgen.py
@@ -3,6 +3,8 @@
 
 import random
 
+from lesson3 import phoneticAlphabet
+
 # word list
 # random choice
 # concatenation
@@ -19,6 +21,47 @@ def randomWord(wordlist):
     word = random.choice(wordlist)
     word = word.strip()
     return word
+
+
+def phoneticAl2(msg):
+    d = {
+            "A": "Alpha",
+            "B": "Bravo",
+            "C": "Charlie",
+            "D": "Delta",
+            "E": "Echo",
+            "F": "Foxtrot",
+            "G": "Golf",
+            "H": "Hotel",
+            "I": "India",
+            "J": "Juliet",
+            "K": "Kilo",
+            "L": "Lima",
+            "M": "Mike",
+            "N": "November",
+            "O": "Oscar",
+            "P": "Papa",
+            "Q": "Quebec",
+            "R": "Romeo",
+            "S": "Sierra",
+            "T": "Tango",
+            "U": "Uniform",
+            "V": "Victor",
+            "W": "Whiskey",
+            "X": "X-ray",
+            "Y": "Yankee",
+            "Z": "Zulu"
+            }
+    lop = []
+    for letter in msg:
+        xl = d.get(letter.upper(), letter)
+        if letter.upper() == letter:
+            lop.append("Capital " + xl)
+        else:
+            lop.append(xl)
+    op = " . ".join(lop)
+    # op = " . ".join(d.get(letter.upper(), letter) for letter in msg)
+    return op
 
 
 def genpw():
@@ -45,6 +88,8 @@ def genpw():
         pwlist.append("".join([xw, yw]))
     pw = "".join(pwlist)
     print(pw)
+    # print(phoneticAlphabet(pw))
+    print(phoneticAl2(pw))
 
 
 if __name__ == "__main__":
