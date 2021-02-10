9421bac135359b0744ee49eec7b24acaa0544cde
ccdale <chris.allison@hivehome.com>
2021-02-10 13:40:54 +0000

    s3.py

---
+++ hublogs/hublogs/s3.py
@@ -0,0 +1,68 @@
+"""S3 routines"""
+
+import boto3
+
+def getMatchingS3Objects(bucket, prefix='', suffix=''):
+    """
+    Generate objects in an S3 bucket.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch objects whose key starts with
+        this prefix (optional).
+    :param suffix: Only fetch objects whose keys end with
+        this suffix (optional).
+    """
+    try:
+        s3 = boto3.client('s3')
+        kwargs = {'Bucket': bucket}
+        # If the prefix is a single string (not a tuple of strings), we can
+        # do the filtering directly in the S3 API.
+        if isinstance(prefix, str):
+            kwargs['Prefix'] = prefix
+        while True:
+            # The S3 API response is a large blob of metadata.
+            # 'Contents' contains information about the listed objects.
+            resp = s3.list_objects_v2(**kwargs)
+            try:
+                contents = resp['Contents']
+            except KeyError:
+                return
+            for obj in contents:
+                key = obj['Key']
+                if key.startswith(prefix) and key.endswith(suffix):
+                    yield obj
+            # The S3 API is paginated, returning up to 1000 keys at a time.
+            # Pass the continuation token into the next response, until we
+            # reach the final page (when this field is missing).
+            try:
+                kwargs['ContinuationToken'] = resp['NextContinuationToken']
+            except KeyError:
+                break
+    except Exception as e:
+    exci = sys.exc_info()[2]
+    lineno = exci.tb_lineno
+    fname = exci.tb_frame.f_code.co_name
+    ename = type(e).__name__
+    msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+    print(msg)
+    raise
+
+def getMatchingS3Keys(bucket, prefix='', suffix=''):
+    """
+    Generate the keys in an S3 bucket.
+
+    :param bucket: Name of the S3 bucket.
+    :param prefix: Only fetch keys that start with this prefix (optional).
+    :param suffix: Only fetch keys that end with this suffix (optional).
+    """
+    try:
+        for obj in getMatchingS3Objects(bucket, prefix, suffix):
+            yield obj['Key']
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+        print(msg)
+        raise
