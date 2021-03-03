25c7f0793d780faca23dfe4f18601e6f4f08d161
ccdale <chris.allison@hivehome.com>
2021-03-03 15:40:03 +0000

    added a timeout to waitForFiles

--- hublogs/hublogs/wait.py
+++ hublogs/hublogs/wait.py
@@ -45,25 +45,33 @@ def getHubListing(bucket, hubid):
 
 # TODO we will need a timeout to cancel this wait should the files not appear
 # after timeout seconds.
-def waitForFiles(bucket, hubid):
+def waitForFiles(bucket, hubid, timeout=600):
     """Wait for files to appear in the bucket that are dated within the last hour."""
     try:
         now = int(time.time())
         now = now - 3600
+        sleeptime = 10
         found = False
         cn = 0
+        print()
         while not found:
             cn += 1
-            print(f"Iteration {cn}")
+            tcn = (cn * sleeptime) - sleeptime
+            if tcn > timeout:
+                raise Exception("Timeout triggered - file did not appear.")
+            print(". ", end="", flush=True)
+            # print(f"Iteration {cn}")
             fns = getHubListing(bucket, hubid)
             for fn in fns:
                 tmp = fn.split("/")
                 datestr = tmp[1]
-                idate = convertTimeStringToTimeStamp(datestr)
-                if idate > now:
-                    found = True
-                    break
-            time.sleep(10)
+                if len(datestr) > 0:
+                    idate = convertTimeStringToTimeStamp(datestr)
+                    if idate > now:
+                        found = True
+                        break
+            time.sleep(sleeptime)
+        print()
         print(fn)
     except Exception as e:
         exci = sys.exc_info()[2]
